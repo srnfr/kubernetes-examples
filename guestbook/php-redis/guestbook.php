ff5f95a9872ca9fbdc4d44520737670ddf826d80
Stephane REYTAN <stephane@reytan.fr>
2023-02-28 17:26:18 +0000

    Change to sentinel

--- guestbook/php-redis/guestbook.php
+++ guestbook/php-redis/guestbook.php
@@ -8,30 +8,28 @@ require 'Predis/Autoloader.php';
 Predis\Autoloader::register();
 
 if (isset($_GET['cmd']) === true) {
-  $host = 'redis-master';
+  $host = 'redis-sentinel';
   if (getenv('GET_HOSTS_FROM') == 'env') {
     $host = getenv('REDIS_MASTER_SERVICE_HOST');
   }
   header('Content-Type: application/json');
   if ($_GET['cmd'] == 'set') {
-    $client = new Predis\Client([
-      'scheme' => 'tcp',
-      'host'   => $host,
-      'port'   => 6379,
-    ]);
+    $client = new Predis\Client(
+      ['tcp://$host']
+      [ 'replication' => 'sentinel', 'service' => 'mymaster' ]
+    );
 
     $client->set($_GET['key'], $_GET['value']);
     print('{"message": "Updated"}');
   } else {
-    $host = 'redis-replica';
+    $host = 'redis';
     if (getenv('GET_HOSTS_FROM') == 'env') {
       $host = getenv('REDIS_SLAVE_SERVICE_HOST');
     }
-    $client = new Predis\Client([
-      'scheme' => 'tcp',
-      'host'   => $host,
-      'port'   => 6379,
-    ]);
+    $client = new Predis\Client(
+      ['tcp://$host']
+      [ 'replication' => 'sentinel' ]
+    );
 
     $value = $client->get($_GET['key']);
     print('{"data": "' . $value . '"}');
